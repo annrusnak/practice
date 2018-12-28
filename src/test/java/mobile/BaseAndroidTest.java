becebf60e60ff964ca12b16f3734a61668132832
Ganna_Rusnak <Ganna_Rusnak@epam.com>
2018-12-28 13:59:10 +0200

    log4j added

--- src/test/java/mobile/BaseAndroidTest.java
+++ src/test/java/mobile/BaseAndroidTest.java
@@ -3,18 +3,15 @@ package mobile;
 import driver.DriverSettings;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import pageObjects.androidPageObjects.BaseAndroidPO;
 import pageObjects.androidPageObjects.LoginPO;
 
 public class BaseAndroidTest {
 
-    protected LoginPO loginPO;
+    LoginPO loginPO;
 
     @BeforeTest
     public void initDriver() {
         loginPO = new LoginPO();
-        //PageObjects init
     }
 
     @AfterTest
